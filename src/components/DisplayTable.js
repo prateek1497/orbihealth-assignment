850e50d22d16310ef93d39e51fada77d91c8537b
Prateek Awasthi <prateek1497@gmail.com>
2020-10-26 18:39:46 +0530

    unused

--- src/components/DisplayTable.js
+++ src/components/DisplayTable.js
@@ -42,32 +42,7 @@ import MUIDataTable from "mui-datatables";
             },
           ];
 
-          const data = [
-            {
-              name: "Joe James",
-              company: "Test Corp",
-              city: "Yonkers",
-              state: "NY",
-            },
-            {
-              name: "John Walsh",
-              company: "Test Corp",
-              city: "Hartford",
-              state: "CT",
-            },
-            {
-              name: "Bob Herm",
-              company: "Test Corp",
-              city: "Tampa",
-              state: "FL",
-            },
-            {
-              name: "James Houston",
-              company: "Test Corp",
-              city: "Dallas",
-              state: "TX",
-            },
-          ];
+         
         
         const {loanAmount, interestRate, yearValue} = this.props
         const arr = []
